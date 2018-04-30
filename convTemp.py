3fcaa10d4043d1790e21274bd4835ab6ba02fbcf
Matthew Freeman <matthew.freeman821@gmail.com>
2018-04-30 12:20:25 -0400

    Converts degrees in Celisius to Fahrenheit

---
+++ convTemp.py
@@ -0,0 +1,5 @@
+def tempConvert(tempInC):
+
+    degreeConvertCtoF = (tempInC * (9/5)) + 32
+    #print('That is', degreeConvertCtoF, 'F.')
+    return degreeConvertCtoF
---
+++ convTempTest.py
@@ -0,0 +1,5 @@
+from convTemp import *
+
+print(tempConvert(25))
+print(tempConvert(15))
+print(tempConvert(0))
